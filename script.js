f89ab17ace13e452220c4b19ae36afff936eebcc
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2023-08-10 16:20:26 +0000

    Added an extra disclaimer + the website now shows the device OS

--- script.js
+++ script.js
@@ -4,10 +4,25 @@ const deviceMemory = document.getElementById("deviceMemory");
 const batteryLevel = document.getElementById("batteryLevel");
 const isFullscreen = document.getElementById("isFullscreen");
 const networkConnectionType = document.getElementById("networkType");
+const deviceOS = document.getElementById("deviceOS");
+let userDetails = navigator.userAgent;
+
+console.log(userDetails)
+
+const os = [
+  { name: "Android", value: "Android" },
+  { name: "iPhone", value: "iPhone" },
+  { name: "iPad", value: "Mac" },
+  { name: "Macintosh", value: "Mac" },
+  { name: "Linux", value: "Linux" },
+  { name: "Windows", value: "Win" },
+];
 
 deviceMemory.innerHTML = `Device Memory: At least ${navigator.deviceMemory} GiB of RAM`;
 
+
 navigator.storage.estimate().then((estimate) => {
+	
 	deviceStorage.innerHTML =
 		`Total Device Storage Estimate: ${(estimate.quota * 0.000001).toFixed(2)} MB`
 });
@@ -15,14 +30,18 @@ navigator.storage.estimate().then((estimate) => {
 getBatteryLevel()
 
 if (navigator.connection === undefined || !networkConnectionType) {
-	networkConnectionType.innerHTML = "Network Type: Unsupported on your browser"
+	networkConnectionType.innerHTML = "Network Type: Unsupported on your browser."
 } else {
 	const networkType = navigator.connection.type
-	networkConnectionType.innerHTML = `Network Type: ${networkType}`
+	if(networkType === "wifi"){
+		networkConnectionType.innerHTML = `Network Type: WiFi`
+	} else {
+		networkConnectionType.innerHTML = `Network Type: ${networkType}`
+	}
 }
 
 async function getBatteryLevel() {
-	if (!navigator.getBattery) {
+	if (!navigator.getBattery || navigator.getBattery === undefined) {
 		batteryLevel.textContent = "Battery Level: Unsupported on your browser";
 	} else {
 		const battery = await navigator.getBattery();
@@ -43,14 +62,32 @@ async function revealIpAddress() {
 	if (document.getElementById("showIpAddress")) {
 		const revealIpAddress = document.getElementById("showIpAddress");
 		revealIpAddress.parentNode.removeChild(revealIpAddress);
+		try {
 		await axios.get(`https://api.valiantwind.dev/v1/get-ip-address`).then((response) => {
 			document.getElementById("ipAddress").innerHTML = "IP Address: " + response.data
 		}).catch((error) => {
 			console.log(error)
-			document.getElementById("ipAddress").innerHTML = "IP Address: Could not fetch IP Address"
+			document.getElementById("ipAddress").innerHTML = `IP Address: Could not fetch IP Address.`
 		})
+		} catch (e){
+			console.log(e)
+		}
 	}
 }
+
+async function getOS(){
+	
+	 for (let i in os) {
+    //check if string contains any value from the object
+    if (userDetails.includes(os[i].value)) {
+      //displau name of OS from the object
+      deviceOS.innerHTML = `Device OS: ${os[i].name}`;
+      break;
+    }
+  }
+}
+
+getOS();
 function createRipple(event) {
 	const button = event.currentTarget;
 
