545816e1e73253105426b521d63ae10ff95bfe52
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2024-01-29 18:14:02 +0000

    Added another field to check if cookies are enabled, and one that tells you your screen resolution and browser name. Rewrote the function that gets your deviceOS to be more accurate + misc bug fixes and optimizations

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-const buttons = document.getElementsByTagName("button");
 const deviceStorage = document.getElementById("deviceStorage");
 const deviceMemory = document.getElementById("deviceMemory");
 const batteryLevel = document.getElementById("batteryLevel");
-const isFullscreen = document.getElementById("isFullscreen");
+const resolution = document.getElementById("screenResolution");
 const networkConnectionType = document.getElementById("networkType");
 const deviceOS = document.getElementById("deviceOS");
-let userDetails = navigator.userAgent;
+const browserName = document.getElementById("browserName");
+const browserCookiedEnabled = document.getElementById("cookiedEnabled")
 
-console.log(userDetails)
 
-const os = [
-  { name: "Android", value: "Android" },
-  { name: "iPhone", value: "iPhone" },
-  { name: "iPad", value: "Mac" },
-  { name: "Macintosh", value: "Mac" },
-  { name: "Linux", value: "Linux" },
-  { name: "Windows", value: "Win" },
-];
 
+let userAgent = navigator.userAgent;
+
+batteryLevel.innerHTML = `Battery Level: ${getBatteryLevel()}%`
 deviceMemory.innerHTML = `Device Memory: At least ${navigator.deviceMemory} GiB of RAM`;
+deviceOS.innerHTML = `OS: ${getOS()};`
+browserName.innerHTML = `Browser: ${getBrowser()};`
+resolution.innerHTML = `Resolution: ${getResolution()}`;
+browserCookiedEnabled.innerHTML = `Browser Cookies Enabled: ${cookiesEnabled()}`;
 
-function megabytesToGigabytes(megabytes) {
-  return megabytes / 1024;
-}
 
 navigator.storage.estimate().then((estimate) => {
 	console.log(estimate.quota);
@@ -31,7 +26,6 @@ navigator.storage.estimate().then((estimate) => {
 		`Total Device Storage Estimate: ${megabytesToGigabytes(estimate.quota * 0.000001).toFixed(2)} GB`
 });
 
-getBatteryLevel()
 
 if (navigator.connection === undefined || !networkConnectionType) {
 	networkConnectionType.innerHTML = "Network Type: Unsupported on your browser."
@@ -47,12 +41,12 @@ if (navigator.connection === undefined || !networkConnectionType) {
 
 async function getBatteryLevel() {
 	if (!navigator.getBattery || navigator.getBattery === undefined) {
-		batteryLevel.textContent = "Battery Level: Unsupported on your browser";
+		return "Unsupported on your browser";
 	} else {
 		const battery = await navigator.getBattery();
 		const level = battery.level;
 		if (batteryLevel) {
-			batteryLevel.innerHTML = `Battery Level: ${level * 100}%`
+			return level * 100
 		}
 	}
 }
@@ -81,40 +75,144 @@ function revealIpAddress() {
 	}
 }
 
-async function getOS(){
-	
-	 for (let i in os) {
-    //check if string contains any value from the object
-    if (userDetails.includes(os[i].value)) {
-      //displau name of OS from the object
-      deviceOS.innerHTML = `Device OS: ${os[i].name}`;
-      break;
-    }
-  }
-}
+// navigator.cookiesEnabled is not accurate for all browsers, so we'll attempt to create a temporary cookie to see if cookies are enabled
+async function cookiesEnabled(){
+	document.cookie = "testcookie=12345678";
 
-getOS();
-function createRipple(event) {
-	const button = event.currentTarget;
-
-	const circle = document.createElement("span");
-	const diameter = Math.max(button.clientWidth, button.clientHeight);
-	const radius = diameter / 2;
-
-	circle.style.width = circle.style.height = `${diameter}px`;
-	circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-	circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-	circle.classList.add("ripple");
-
-	const ripple = button.getElementsByClassName("ripple")[0];
-
-	if (ripple) {
-		ripple.remove();
+	if(document.cookie.indexOf("testcookie=12345678") >= 0){
+		return true;
 	}
 
-	button.appendChild(circle);
+	return false;
 }
 
-for (const button of buttons) {
-	button.addEventListener("click", createRipple);
+async function getResolution(){
+	const screen = window.screen;
+	return `${screen.width}x${screen.height}`;
+}
+
+async function getBrowser(){
+	if(userAgent.indexOf("Chrome") >= 0){
+		if (userAgent.match(/\bChrome\/[.0-9]* Mobile\b/)) {
+				if (userAgent.match(/\bVersion\/\d+\.\d+\b/) || userAgent.match(/\bwv\b/)) {
+						return "WebView on Android"
+				} else {
+						return "Google Chrome for Android"
+				}
+		} else {
+				return "Google Chrome"
+		}
+	}
+
+	if(userAgent.indexOf("Edge") >= 0){
+		return "Microsoft Edge";
+	}
+
+	if (userAgent.indexOf("Firefox") >= 0 && userAgent.indexOf("Seamonkey") === -1) {
+			if (userAgent.indexOf("Android") >= 0) {
+					return "Firefox for Android";
+			} else {
+					return "Firefox"
+			}
+	}
+
+	if (userAgent.indexOf("OPR") >= 0 || userAgent.indexOf("Opera") >= 0) {
+			if (userAgent.indexOf("Opera Mobi") >= 0 || userAgent.indexOf("Opera Tablet") >= 0 || userAgent.indexOf("Mobile") >= 0) {
+					return "Opera Mobile";
+			} else if (userAgent.indexOf("Opera Mini") >= 0) {
+					return "Opera Mini";
+			} else {
+					return "Opera";
+			}
+	}
+
+	if (userAgent.indexOf("Safari") >= 0 && userAgent.indexOf("Chrome") === -1 && userAgent.indexOf("Chromium") === -1 && userAgent.indexOf("Android") === -1) {
+			if (userAgent.indexOf("CriOS") >= 0) {
+					return "Chrome for iOS";
+			} else if (userAgent.indexOf("FxiOS") >= 0) {
+					return "Firefox for iOS";
+			} else {
+					return "Safari";
+			}
+	}
+
+	if (userAgent.indexOf("Trident") >= 0 || userAgent.indexOf("MSIE") >= 0) {
+			if (userAgent.indexOf("Mobile") >= 0) {
+					return "IE Mobile";
+			} else {
+					return "Internet Explorer";
+			}
+	}
+
+	if (userAgent.indexOf("Android") >= 0 && userAgent.indexOf("Chrome") === -1 && userAgent.indexOf("Chromium") === -1 && userAgent.indexOf("Trident") === -1 && userAgent.indexOf("Firefox") === -1) {
+			return "Android Browser";
+	}
+
+	if (userAgent.indexOf("BB10") >= 0 || userAgent.indexOf("PlayBook") >= 0 || userAgent.indexOf("BlackBerry") >= 0) {
+			return "BlackBerry";
+	}
+
+	if (userAgent.indexOf("UCBrowser") >= 0) {
+			return "UC Browser for Android";
+	}
+
+	if (userAgent.indexOf("SamsungBrowser") >= 0) {
+			return "Samsung Internet";
+	}
+
+	if (userAgent.indexOf("MQQBrowser") >= 0) {
+			return "QQ Browser";
+	}
+
+	return "Could not detect browser";
+}
+
+async function getOS(){
+	if (userAgent.indexOf("Windows") >= 0) {
+			if (userAgent.indexOf("Windows Phone") >= 0) {
+					return "Windows Phone";
+			} else {
+					return "Windows";
+			}
+	}
+
+	if (userAgent.indexOf("OS X") >= 0 && userAgent.indexOf("Android") === -1) {
+			return "macOS";
+	}
+
+	if (userAgent.indexOf("Linux") >= 0) {
+			return "Linux";
+	}
+
+	if ((userAgent.indexOf("Android") >= 0 || userAgent.indexOf("Adr") >= 0) && userAgent.indexOf("Windows Phone") === -1) {
+			return "Android";
+	}
+
+	if (userAgent.indexOf("like Mac OS X") >= 0) {
+			return "iOS";
+	}
+
+	if (userAgent.indexOf("CrOS") >= 0) {
+			return "Chrome OS";
+	}
+
+	if (userAgent.indexOf("BB10") >= 0) {
+			return "BlackBerry";
+	}
+
+	if (userAgent.indexOf("RIM Tablet OS") >= 0) {
+			return "BlackBerry Tablet OS";
+	}
+
+	if (userAgent.indexOf("BlackBerry") >= 0) {
+			return "BlackBerryOS";
+	}
+
+	if (userAgent.indexOf("KAIOS") >= 0) {
+			return "KaiOS";
+	}
+}
+
+function megabytesToGigabytes(megabytes) {
+	return megabytes / 1024;
 }
