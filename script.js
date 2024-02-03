4741827c08fd61edc91c7e890c14bb06019d854e
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2024-02-03 08:20:39 +0000

    Let's just say...massive improvements have been made

--- script.js
+++ script.js
@@ -4,38 +4,81 @@ const resolution = document.getElementById("screenResolution");
 const networkConnectionType = document.getElementById("networkType");
 const deviceOS = document.getElementById("deviceOS");
 const browserName = document.getElementById("browserName");
+const browserVersion = document.getElementById("browserVersion");
+const devicePlatform = document.getElementById("devicePlatform");
 const browserCookiedEnabled = document.getElementById("cookiedEnabled");
 let batteryLevel = document.querySelector("#batteryLevel");
 let batteryStatus = document.querySelector("#batteryCharging");
 
 let userAgent = navigator.userAgent;
 
-deviceOS.innerHTML = `OS: ${getOS()}`;
-browserCookiedEnabled.innerHTML = `Browser Cookies Enabled: ${cookiesEnabled()}`;
+getOSAsync().then(os => {
+	deviceOS.textContent = `OS: ${os}`;
+});
+
+getPlatformAsync().then(platform => {
+	devicePlatform.textContent = `Platform: ${platform}`;
+})
+
+getBrowserAsync().then((info) => {
+	browserName.textContent = `Browser: ${info.browser}`;
+	browserVersion.textContent = `Browser Version: ${info.version}`;
+}).catch((error) => {
+	console.error(error)
+});
 
 getResolutionAsync().then((res) => {
-		resolution.innerHTML = `Resolution: ${res}`;
+	resolution.textContent = `Resolution: ${res}`;
 }).catch((error) => {
-		console.error(error);
+	console.error(error);
 });
-async function getBatteryLevelAsync() {
-	await navigator.getBattery().then((battery) => {
 
-		if (!navigator.getBattery() || navigator.getBattery() === undefined){
-			batteryStatus.textContent = "Battery Status: Unsupported on your browser";
-			batteryLevel.textContent = "Battery Level: Unsupported on your browser";
-		} else {
-				batteryLevel.textContent = `Battery Level: ${battery.level * 100}%`;
+getIpAddressAsync().then(ip => {
+	if (document.getElementById("showIpAddress")) {
 
-			if (battery.charging) {
-				batteryStatus.textContent = `Battery Status: Charging`;
-			} else {
-				batteryStatus.textContent =
-					`Battery Status: Not Charging`;
-			}
-		}
+		const revealIpAddress = document.getElementById("showIpAddress");
 
-		battery.onlevelchange = () => {
+		revealIpAddress.addEventListener("click", function() {
+			revealIpAddress.parentNode.removeChild(revealIpAddress);
+			document.getElementById("ipAddress").textContent = `IP Address: ${ip}`;
+		})
+	}
+}).catch(e => {
+	console.error(e)
+	document.getElementById("ipAddress").textContent = `IP Address: Could not fetch IP Address.`
+})
+
+displayBatteryLevelAsync()
+
+
+browserCookiedEnabled.textContent = `Browser Cookies Enabled: ${cookiesEnabled()}`;
+
+
+if (navigator.deviceMemory === undefined) {
+	deviceMemory.textContent = `Device Memory: Unsupported on your browser`;
+} else {
+	deviceMemory.textContent = `Device Memory: At least ${navigator.deviceMemory} GiB of RAM`;
+}
+
+
+if (navigator.connection === undefined || !networkConnectionType) {
+	networkConnectionType.textContent = "Network Type: Unsupported on your browser"
+} else {
+	let networkType = navigator.connection.type
+	console.log(networkType);
+
+	networkType = networkType.substring(0, 1).toUpperCase() + networkType.substring(1);
+	networkConnectionType.textContent = `Network Type: ${networkType}`
+}
+
+async function displayBatteryLevelAsync() {
+	if (!navigator.getBattery() || (navigator.getBattery() === undefined) || navigator.getBattery === undefined) {
+		batteryStatus.textContent = "Battery Status: Unsupported on your browser";
+		batteryLevel.textContent = "Battery Level: Unsupported on your browser";
+		return;
+	} else {
+		await navigator.getBattery().then((battery) => {
+
 			batteryLevel.textContent = `Battery Level: ${battery.level * 100}%`;
 
 			if (battery.charging) {
@@ -44,88 +87,46 @@ async function getBatteryLevelAsync() {
 				batteryStatus.textContent =
 					`Battery Status: Not Charging`;
 			}
-		};
-	}).catch((error) => {
-		console.log(error)	
-	});
-}
+		},
 
-getBatteryLevelAsync()
+			battery.onlevelchange = () => {
+				batteryLevel.textContent = `Battery Level: ${battery.level * 100}%`;
 
-getBrowserAsync().then((browser) => {
-	browserName.innerHTML = `Browser: ${browser}`;
-}).catch((error) => {
-	console.error(error)
-});
-
-// navigator.storage.estimate().then((estimate) => {
-// 	console.log(estimate.quota);
-	
-// 	deviceStorage.innerHTML =
-// 		// `Total Device Storage Estimate: ${(estimate.quota * 0.000001).toFixed(2)} MB`
-// 		// `Total Device Storage Estimate: ${megabytesToGigabytes(estimate.quota * 0.000001).toFixed(2)} GB`
-
-// 		((estimate.usage / estimate.quota) * 100).toFixed(2);
-// });
-
-if(navigator.deviceMemory === undefined){
-	deviceMemory.innerHTML = `Device Memory: Unsupported on your browser`;
-} else {
-	deviceMemory.innerHTML = `Device Memory: At least ${navigator.deviceMemory} GiB of RAM`;
-}
-
-
-if (navigator.connection === undefined || !networkConnectionType) {
-	networkConnectionType.innerHTML = "Network Type: Unsupported on your browser"
-} else {
-	let networkType = navigator.connection.type
-	console.log(networkType);
-
-	networkType = networkType.substring(0, 1).toUpperCase() + networkType.substring(1);
-		networkConnectionType.innerHTML = `Network Type: ${networkType}`
-}
-
-// async function getBatteryLevelAsync() {
-// 	if (!navigator.getBattery || navigator.getBattery === undefined) {
-// 		return "Unsupported on your browser";
-// 	} else {
-// 		const battery = await navigator.getBattery();
-// 		const level = battery.level;
-// 		if (batteryLevel) {
-// 			return level * 100
-// 		}
-// 	}
-// }
-
-function locationDetails() {
-	if (document.getElementById("locationDetails")) {
-		window.location.href = "https://ValiantWind.github.io/Location-Details"
+				if (battery.charging) {
+					batteryStatus.textContent = `Battery Status: Charging`;
+				} else {
+					batteryStatus.textContent =
+						`Battery Status: Not Charging`;
+				}
+			}).catch((error) => {
+				console.log(error)
+			});
 	}
+
 }
 
-function revealIpAddress() {
-	if (document.getElementById("showIpAddress")) {
-		const revealIpAddress = document.getElementById("showIpAddress");
-		revealIpAddress.parentNode.removeChild(revealIpAddress);
+
+async function getIpAddressAsync() {
+	return new Promise((resolve, reject) => {
 		try {
 			axios.get(`https://api.valiantwind.dev/v1/get-ip-address`).then((response) => {
-			document.getElementById("ipAddress").innerHTML = "IP Address: " + response.data
-		}).catch((error) => {
-			console.log(error)
-			console.log(response.data)
-			document.getElementById("ipAddress").innerHTML = `IP Address: Could not fetch IP Address.`
-		})
-		} catch (e){
-			console.log(e)
+				resolve(response.data)
+			}).catch((error) => {
+				console.log(error)
+				console.log(response.data)
+				reject(error)
+			})
+		} catch (e) {
+			reject(e)
 		}
-	}
+	})
 }
 
 // navigator.cookiesEnabled is not accurate for all browsers, so we'll attempt to create a temporary cookie to see if cookies are enabled
-function cookiesEnabled(){
+function cookiesEnabled() {
 	document.cookie = "testcookie=12345678";
 
-	if(document.cookie.indexOf("testcookie=12345678") >= 0){
+	if (document.cookie.indexOf("testcookie=12345678") >= 0) {
 		return true;
 	}
 
@@ -134,146 +135,84 @@ function cookiesEnabled(){
 	return false;
 }
 
-function getResolutionAsync() {
-		return new Promise((resolve, reject) => {
-				try {
-						const screen = window.screen;
-						const resolution = `${screen.width}x${screen.height}`;
-						resolve(resolution);
-				} catch (error) {
-						reject(error);
-				}
-		});
-}
-
-function getBrowserAsync(){
+async function getResolutionAsync() {
 	return new Promise((resolve, reject) => {
 		try {
-
-			if(userAgent.indexOf("Edge") >= 0){
-				resolve("Microsoft Edge")
-			}
-
-			
-		if(userAgent.indexOf("Chrome") >= 0){
-			if (userAgent.match(/\bChrome\/[.0-9]* Mobile\b/)) {
-					if (userAgent.match(/\bVersion\/\d+\.\d+\b/) || userAgent.match(/\bwv\b/)) {
-						resolve("WebView for Android")
-					} else {
-						resolve("Google Chrome for Android")
-					}
-			} else {
-					resolve("Google Chrome");
-			}
+			const screen = window.screen;
+			const resolution = `${screen.width}x${screen.height}`;
+			resolve(resolution);
+		} catch (error) {
+			reject(error);
 		}
+	});
+}
 
-		if (userAgent.indexOf("Firefox") >= 0 && userAgent.indexOf("Seamonkey") === -1) {
-				if (userAgent.indexOf("Android") >= 0) {
-						resolve("Firefox for Android")
-				} else {
-						resolve("Firefox")
-				}
-		}
 
-		if (userAgent.indexOf("OPR") >= 0 || userAgent.indexOf("Opera") >= 0) {
-				if (userAgent.indexOf("Opera Mobi") >= 0 || userAgent.indexOf("Opera Tablet") >= 0 || userAgent.indexOf("Mobile") >= 0) {
-						resolve("Opera Mobile")
-				} else if (userAgent.indexOf("Opera Mini") >= 0) {
-						resolve("Opera Mini")
-				} else {
-						resolve("Opera")
-				}
-		}
+// Server-sided solution using my API. You can only detect so much when on the client. 
+// Its definitely possible to detect the browser on the client, but utilizing the server-sided solution is more reliable and accurate.
+// The code used to be much longer when I used a client-sided solution for detecting device info.
 
-		if (userAgent.indexOf("Safari") >= 0 && userAgent.indexOf("Chrome") === -1 && userAgent.indexOf("Chromium") === -1 && userAgent.indexOf("Android") === -1) {
-				if (userAgent.indexOf("CriOS") >= 0) {
-					resolve("Chrome for iOS")
-				} else if (userAgent.indexOf("FxiOS") >= 0) {
-						resolve("Firefox for iOS")
-				} else {
-						resolve("Safari")
-				}
-		}
+async function getSystemInfoAsync() {
+	try {
+		const response = await fetch(`https://api.valiantwind.dev/v1/system-info`)
+		return await response.json()
+	} catch (e) {
+		console.error(e)
+	}
+}
 
-		if (userAgent.indexOf("Trident") >= 0 || userAgent.indexOf("MSIE") >= 0) {
-				if (userAgent.indexOf("Mobile") >= 0) {
-						resolve("IE Mobile")
-				} else {
-						resolve("Internet Explorer")
-				}
-		}
 
-		if (userAgent.indexOf("Android") >= 0 && userAgent.indexOf("Chrome") === -1 && userAgent.indexOf("Chromium") === -1 && userAgent.indexOf("Trident") === -1 && userAgent.indexOf("Firefox") === -1) {
-				resolve("Android Browser");
-		}
-
-		if (userAgent.indexOf("BB10") >= 0 || userAgent.indexOf("PlayBook") >= 0 || userAgent.indexOf("BlackBerry") >= 0) {
-				resolve("BlackBerry");
-		}
-
-		if (userAgent.indexOf("UCBrowser") >= 0) {
-				resolve("UC Browser for Android");
-		}
-
-		if (userAgent.indexOf("SamsungBrowser") >= 0) {
-				resolve("Samsung Internet")
-		}
-
-		if (userAgent.indexOf("MQQBrowser") >= 0) {
-				resolve("QQ Browser");
-		}
+async function getBrowserAsync() {
+	return new Promise((resolve, reject) => {
+		try {
+			getSystemInfoAsync().then(info => {
+				const browser = info.browser;
+				const version = info.version;
+				resolve({ browser, version })
+			})
 		} catch (e) {
-			reject(e);	
+			reject(e)
+		}
+	})
+
+}
+
+async function getOSAsync() {
+	return new Promise((resolve, reject) => {
+		try {
+			getSystemInfoAsync().then(info => {
+				if (info.isiPhone) {
+					resolve("iOS")
+				} else if (info.isiPad) {
+					resolve("iPadOS")
+				} else {
+					resolve(info.os)
+				}
+			})
+		} catch (e) {
+			reject(e)
 		}
 	})
 }
 
-function getOS(){
-	if (userAgent.indexOf("Windows") >= 0) {
-			if (userAgent.indexOf("Windows Phone") >= 0) {
-					return "Windows Phone";
-			} else {
-					return "Windows";
-			}
-	}
-
-	if (userAgent.indexOf("OS X") >= 0 && userAgent.indexOf("Android") === -1) {
-			return "macOS";
-	}
-
-	if (userAgent.indexOf("Linux") >= 0) {
-			return "Linux";
-	}
-
-	if ((userAgent.indexOf("Android") >= 0 || userAgent.indexOf("Adr") >= 0) && userAgent.indexOf("Windows Phone") === -1) {
-			return "Android";
-	}
-
-	if (userAgent.indexOf("like Mac OS X") >= 0) {
-			return "iOS";
-	}
-
-	if (userAgent.indexOf("CrOS") >= 0) {
-			return "Chrome OS";
-	}
-
-	if (userAgent.indexOf("BB10") >= 0) {
-			return "BlackBerry";
-	}
-
-	if (userAgent.indexOf("RIM Tablet OS") >= 0) {
-			return "BlackBerry Tablet OS";
-	}
-
-	if (userAgent.indexOf("BlackBerry") >= 0) {
-			return "BlackBerryOS";
-	}
-
-	if (userAgent.indexOf("KAIOS") >= 0) {
-			return "KaiOS";
-	}
+async function getPlatformAsync() {
+	return new Promise((resolve, reject) => {
+		try {
+			getSystemInfoAsync().then(info => {
+				resolve(info.platform)
+			})
+		} catch (e) {
+			reject(e)
+		}
+	})
 }
 
 function megabytesToGigabytes(megabytes) {
 	return megabytes / 1024;
+}
+
+function locationDetails() {
+	if (document.getElementById("locationDetails")) {
+		window.location.href = "https://ValiantWind.github.io/Location-Details"
+	}
 }
